60b541777426c50b228df393bc36e1e6488c14c4
Kunal Gangani <kunalgangani009@gmail.com>
2026-08-20 13:49:33 +0530

    Replaced Hardcoded with API

--- mobile/swarsetu_app/lib/features/translation/presentation/screens/translation_screen.dart
+++ mobile/swarsetu_app/lib/features/translation/presentation/screens/translation_screen.dart
@@ -7,10 +7,12 @@ import 'package:swarsetu_app/core/theme/app_text_styles.dart';
 import 'package:swarsetu_app/features/translation/bloc/translation_bloc.dart';
 import 'package:swarsetu_app/features/translation/bloc/translation_event.dart';
 import 'package:swarsetu_app/features/translation/bloc/translation_state.dart';
-import 'package:swarsetu_app/features/translation/domain/repositories/mock_repository.dart';
 import 'package:swarsetu_app/features/translation/presentation/widgets/language_selector.dart';
 import 'package:swarsetu_app/features/translation/presentation/widgets/translation_widget_card.dart';
 import 'package:swarsetu_app/features/translation/presentation/widgets/voice_button.dart';
+import 'package:swarsetu_app/core/network/api_client.dart';
+import 'package:swarsetu_app/features/translation/data/datasources/translation_remote_data_source.dart';
+import 'package:swarsetu_app/features/translation/data/repositories/translation_repository_impl.dart';
 
 class TranslationScreen extends StatelessWidget {
   const TranslationScreen({super.key});
@@ -18,8 +20,13 @@ class TranslationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) =>
-          TranslationBloc(repository: MockTranslationRepository()),
+      create: (context) => TranslationBloc(
+        repository: TranslationRepositoryImpl(
+          remoteDataSource: TranslationRemoteDataSourceImpl(
+            apiClient: ApiClient(),
+          ),
+        ),
+      ),
       child: const _TranslationView(),
     );
   }
